75c4dde2dd32c93c1e2ef378943da8f85b0d00d4
Linkon Sid <linkon@sheba.xyz>
2021-03-24 11:05:27 +0600

    new serializer generated for appointment

---
+++ lib/Model/DoctorAppointmentData.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'DoctorAppointmentData.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DoctorAppointmentData _$DoctorAppointmentDataFromJson(
+    Map<String, dynamic> json) {
+  return DoctorAppointmentData(
+    json['code'] as int,
+    json['serialNo'] as int,
+  );
+}
+
+Map<String, dynamic> _$DoctorAppointmentDataToJson(
+        DoctorAppointmentData instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'serialNo': instance.serialNo,
+    };
