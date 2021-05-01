6b86d63e0bc369de98647b27b7ec824b337c2980
Linkon Sid <linkon@sheba.xyz>
2021-05-02 02:06:26 +0600

    push notification structure written for foreground & background

--- lib/Services/PushNotification.dart
+++ lib/Services/PushNotification.dart
@@ -6,7 +6,7 @@ import 'package:patientapp/Model/AppointmentNotificationData.dart';
 
 class PushNotification{
   FirebaseMessaging messaging;
-  FlutterLocalNotificationsPlugin localNotification;
+  FlutterLocalNotificationsPlugin localNotification = FlutterLocalNotificationsPlugin();
 
   static Future<String> getRegToken() async{
     var token = await FirebaseMessaging.instance.getToken();
@@ -24,17 +24,20 @@ class PushNotification{
     var generalNotificationDetails = NotificationDetails(android: androidDetails,iOS: iOSDetails);
     await localNotification.show(0, 'appointment ${data.id}', data.message, generalNotificationDetails);
   }
-  Future initMessaging() async{
-    await Firebase.initializeApp();
+  initMessaging(){
     messaging = FirebaseMessaging.instance;
+  }
+  Future init() async{
+    await Firebase.initializeApp();
+    initMessaging();
     this.notificationPermission();
+    this.initSettings();
+  }
+  initSettings(){
     var androidInit = AndroidInitializationSettings('@mipmap/ic_launcher');
     var iosInit = IOSInitializationSettings();
     var initSettings = InitializationSettings(android: androidInit,iOS: iosInit);
-    localNotification = FlutterLocalNotificationsPlugin();
     localNotification.initialize(initSettings);
-    foregroundNotification();
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   }
   void foregroundNotification(){
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
@@ -52,36 +55,4 @@ class PushNotification{
       sound: true,
     );
   }
-}
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  // print('message id........ ${message.data}');
-  notificationPermission();
-  showNotification(AppointmentNotificationData.fromJson(message.data));
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
-  // this.showNotification(AppointmentNotificationData.fromJson(message.data));
-}
-void showNotification(AppointmentNotificationData data) async{
-  var androidInit = AndroidInitializationSettings('@mipmap/ic_launcher');
-  var iosInit = IOSInitializationSettings();
-  var initSettings = InitializationSettings(android: androidInit,iOS: iosInit);
-  var localNotification = FlutterLocalNotificationsPlugin();
-  localNotification.initialize(initSettings);
-  var androidDetails = AndroidNotificationDetails('channelId', 'channelName', 'channelDescription');
-  var iOSDetails = IOSNotificationDetails();
-  var generalNotificationDetails = NotificationDetails(android: androidDetails,iOS: iOSDetails);
-  await localNotification.show(0, 'appointment ${data.id}', data.message, generalNotificationDetails);
-}
-void notificationPermission() async{
-  var messaging = FirebaseMessaging.instance;
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:patientapp/Views/HospitalListView.dart';
 import 'package:patientapp/Views/LoginView.dart';
+import 'Model/AppointmentNotificationData.dart';
 import 'Services/PushNotification.dart';
 import 'Views/BaseView.dart';
 import 'package:patientapp/Helper/Cache.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await PushNotification().initMessaging();
+  var notification = PushNotification();
+  await notification.init();
+  notification.foregroundNotification();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(Main());
 }
 
@@ -27,3 +33,12 @@ class Main extends StatelessWidget {
   }
 }
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  var notification = PushNotification();
+  await notification.init();
+  // print('message id........ ${message.data}');
+  notification.showNotification(AppointmentNotificationData.fromJson(message.data));
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  // this.showNotification(AppointmentNotificationData.fromJson(message.data));
+}
